2fb96e1582da41d2c02392b4711ecaf9e8e7487f
luqman316 <luqmanrehan88@gmail.com>
2025-08-05 18:17:03 +0500

    luqman

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,59 +1,8 @@
-// "use client";
-// import { projects } from "@/data/projects";
-// import { motion } from "framer-motion";
-// import Link from "next/link";
-// import ProjectLayout from "./ProjectLayout";
-
-// // projects are now imported from @/data/projects
-// type ProjectsProps = {
-//   limit?: number;
-// };
-
-// function Projects({ limit }: ProjectsProps) {
-//   return (
-//     <motion.div
-//       initial={{ opacity: 0 }}
-//       whileInView={{ opacity: 1 }}
-//       transition={{ duration: 0.5 }}
-//       className="container mx-auto px-16  sm:px-4 py-6 sm:py-8 text-white"
-//     >
-      
-//       <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 md:gap-8  sm:mt-8">
-//         {(limit ? projects.slice(0, limit) : projects.slice(0, 8)).map(
-//           (project, index) => (
-//             <Link
-//               key={project.slug}
-//               href={`/projects/${project.slug}`}
-//               className="block"
-//             >
-//               <ProjectLayout
-//                 image={Array.isArray(project.image) ? project.image[0] : project.image}
-//                 title={project.title}
-//                 description={project.description}
-//                 direction={index % 2 === 0 ? "left" : "right"}
-//               />
-//             </Link>
-//           )
-//         )}
-//       </div>
-//       <div className="mt-4 text-center">
-//         <Link href={"/games"}>
-//           <p className="text-xs sm:text-sm md:text-base  animate-bounce text-gray-400 mt-6">
-//             For more projects, click here
-//           </p>
-//         </Link>
-//       </div>
-//     </motion.div>
-//   );
-// }
-
-// export default Projects;
-
 "use client";
 import { projects } from "@/data/projects";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import ProjectLayout from "./ProjectLayout";
+// import ProjectLayout from "./ProjectLayout";
 
 import {
   Carousel,
@@ -64,6 +13,7 @@ import {
 } from "@/components/ui/carousel";
 
 import Autoplay from "embla-carousel-autoplay";
+import ProjectLayout from "@/components/ProjectLayout";
 
 type ProjectsProps = {
   limit?: number;
